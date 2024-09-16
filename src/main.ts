8b13c56fb070994b296a39c61e95ea159e109cd4
Slater <armando.jaimes@pucp.edu.pe>
2024-09-16 00:06:05 -0500

    Agregar las configuraciones de variables de entorno y las configuraciones generales

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
 
   const logger = new Logger('Main Data Analytics')
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(3003);
+  await app.listen(envs.port);
 
-  logger.log(`Data Analytics Microservice running on port ${3003}`)
+  logger.log(`Data Analytics Microservice running on port ${envs.port}`)
 
 }
 bootstrap();
